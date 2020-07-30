a246822fe87b7730c0f5720e0a036e1834b89dbc
deeptinirwal <66492566+deeptinirwal@users.noreply.github.com>
2020-07-30 11:42:04 -0400

    First Commit

---
+++ ApplicantSkillPoco.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CareerCloud.Pocos 
+
+{
+    [Table("Applicant_Skills")]
+    public class ApplicantSkillPoco : IPoco
+    {
+        [Key]
+       public Guid Id { get; set; } 
+
+       public Guid Applicant { get; set; }
+
+    public string Skill { get; set; }
+
+    [Column("Skill_Level")]
+    public string SkillLevel { get; set; }
+   
+    [Column("Start_Month")]
+    public byte StartMonth { get; set; }
+
+    [Column("Start_Year")]
+    public int StartYear { get; set; }
+
+    [Column("End_Month")]
+    public byte EndMonth { get; set; }
+
+    [Column("End_Year")]
+    public int EndYear { get; set; }
+
+    [Column("Time_Stamp")]
+
+    public Byte[] TimeStamp { get; set; }
+        public virtual ApplicantProfilePoco ApplicantProfile { get; set; }
+
+    }
+}
---
+++ CompanyJobPoco.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CareerCloud.Pocos
+{
+    [Table("Company_Jobs")]
+    public class CompanyJobPoco : IPoco
+    {
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid Company { get; set; }
+        
+        [Column("Profile_Created")]
+        public DateTime ProfileCreated { get; set; }
+
+
+        [Column("Is_Inactive")]
+        public Boolean IsInactive { get; set; }
+
+        [Column("Is_Company_Hidden")]
+        public Boolean IsCompanyHidden { get; set; }
+
+        [Column("Time_Stamp")]
+
+        public Byte[] TimeStamp { get; set; }
+
+
+        public virtual ICollection<ApplicantJobApplicationPoco> ApplicantJobApplications { get; set; }
+        public virtual ICollection<CompanyJobSkillPoco> CompanyJobSkills { get; set; }
+        public virtual CompanyProfilePoco CompanyProfile { get; set; }
+        public virtual ICollection<CompanyJobEducationPoco> CompanyJobEducations { get; set; }
+        public virtual ICollection<CompanyJobDescriptionPoco> CompanyJobDescriptions { get; set; }
+
+    }
+}
